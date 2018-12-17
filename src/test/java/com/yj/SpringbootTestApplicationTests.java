5e17ea9cacbfa5fc1ed63cb0d8ae5b14c8702863
yj <2868427862@qq.com>
2018-12-17 19:33:35 +0800

    "springboot-test"

---
+++ src/test/java/com/yj/SpringbootTestApplicationTests.java
@@ -0,0 +1,30 @@
+package com.yj;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootTestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+
+    @Test
+    public void tesLength() {
+        //数组有length属性
+        int[] a = new int[10];
+        System.out.println(a.length);
+        //字符串有length()方法
+        String str = "First Love!";
+        System.out.println(str.length());
+
+
+    }
+
+}
+
